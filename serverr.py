c85b39af0a37e6b832a8f61cae5bc4e47d95301b
Lastonetry <alwaysoptimisticfutureiitian@gmail.com>
2024-08-29 09:16:12 +0530

    Update serverr.py

--- serverr.py
+++ serverr.py
@@ -8,7 +8,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7180504388:AAG4OHitdXwAEyPzivxgKk4NN5ThUzKgJ9k')
+bot = telebot.TeleBot('7492960869:AAHByj6rrPEaJh1nEYT81pFjirg_hMfVdV4')
 
 # Admin user IDs
 admin_id = {"7462351545"}
